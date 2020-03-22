303d48f0335c8d97cb10fe164f74accab4f40aa7
unknown <mohamedashraf892@gmail.com>
2020-03-22 20:16:22 +0200

    first commit

---
+++ Assets/CollisionScript.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class CollisionScript : MonoBehaviour {
+
+  //for this to work both need colliders, one must have rigid body (spaceship) the other must have is trigger checked.
+  void OnTriggerEnter (Collider col)
+  {
+   //GameObject explosion = Instantiate(Resources.Load("FlareMobile", typeof(GameObject))) as GameObject;
+   // explosion.transform.position = transform.position;
+    Destroy(col.gameObject);
+   //Destroy (explosion, 2);
+    Destroy (gameObject);
+  }
+
+}
+
---
+++ Assets/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    void Update()
+    {
+          if (GameObject.FindGameObjectsWithTag("Player").Length <= 1){
+
+      GameObject enemy = Instantiate(Resources.Load("enemy", typeof(GameObject))) as GameObject;
+      GameObject enemy1 = Instantiate(Resources.Load("enemy1", typeof(GameObject))) as GameObject;
+      GameObject enemy2 = Instantiate(Resources.Load("enemy2", typeof(GameObject))) as GameObject;
+      GameObject enemy3 = Instantiate(Resources.Load("enemy3", typeof(GameObject))) as GameObject;
+
+    }
+
+    }
+}
